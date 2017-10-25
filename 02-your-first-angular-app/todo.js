2c688e230c900e8a882556888a3698e5c35b7dcf
Peter Csontos <csontos.peter@ekreta.hu>
2017-10-25 15:31:54 +0200

    Ch02 :: Adatmodell keszitese

---
+++ 02-your-first-angular-app/todo.js
@@ -0,0 +1,9 @@
+var model = {
+    user: "Peter",
+    items: [
+        { action: "Vegyél virágot", done: false },
+        { action: "Vegyél cipőt", done: false },
+        { action: "Vedd fel a jegeket", done: true },
+        { action: "H\vd fel Jóskát!", done: false }
+    ]
+};
