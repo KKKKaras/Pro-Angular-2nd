8473556b3ffa40adf878bbb349831443485d11ed
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-07 12:31:04 +0100

    5.4.1 Defining Functions with Parameters

--- 05 - JavaScript and TypeScript - Part 1/JavaScriptPrimer/primer.ts
+++ 05 - JavaScript and TypeScript - Part 1/JavaScriptPrimer/primer.ts
@@ -1,6 +1,11 @@
-let myFunc = function(name, weather) {
+let myFunc = function(name, weather = "raining", ...extraArgs) {
   console.log("Hello " + name + ".");
   console.log("It is " + weather + " today");
+  for (let i = 0; i < extraArgs.length; i++) {
+    console.log("Extra Arg: " + extraArgs[i]);
+  }
 };
 
 myFunc("Adam", "sunny");
+myFunc("Adam");
+myFunc("Adam", "sunny", "one", "two", "three");
