73c179ec59a9761b882a6145d550665ee4d70354
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-07 13:48:05 +0100

    5.5 Using Variables and Types

--- 05 - JavaScript and TypeScript - Part 1/JavaScriptPrimer/primer.ts
+++ 05 - JavaScript and TypeScript - Part 1/JavaScriptPrimer/primer.ts
@@ -1,8 +1,27 @@
-let myFunc = nameFunction => "Hello " + nameFunction() + ".";
+let messageFunction = function(name, weather) {
+  let message = "Hello, Peter";
+  if (weather == "sunny") {
+    let message = "It is a nice day";
+    console.log(message);
+  } else {
+    let message = "It is " + weather + " today";
+    console.log(message);
+  }
+  console.log(message);
+};
 
-let printName = (nameFunction, printFunction) =>
-  printFunction(myFunc(nameFunction));
+let messageFunctionVar = function(name, weather) {
+  var message = "Hello, Adam";
+  if (weather == "sunny") {
+    var message = "It is a nice day";
+    console.log(message);
+  } else {
+    var message = "It is " + weather + " today";
+    console.log(message);
+  }
+  console.log(message);
+};
 
-printName(function() {
-  return "Peter";
-}, console.log);
+messageFunctionVar("Adam", "raining");
+
+messageFunction("Peter", "raining");
