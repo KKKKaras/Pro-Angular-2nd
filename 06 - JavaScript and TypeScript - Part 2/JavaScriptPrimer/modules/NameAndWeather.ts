0a379e4386c9b9b3c8b1f17465ed07747c2a0206
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-07 15:53:10 +0100

    6.4.1.1 Type Annotating Properties and Variables

--- 06 - JavaScript and TypeScript - Part 2/JavaScriptPrimer/modules/NameAndWeather.ts
+++ 06 - JavaScript and TypeScript - Part 2/JavaScriptPrimer/modules/NameAndWeather.ts
@@ -1,21 +1,21 @@
 export class Name {
-  constructor(first, second) {
+  constructor(private first: string, private second: string) {
     this.first = first;
     this.second = second;
   }
 
-  get nameMessage() {
+  get nameMessage(): string {
     return `Hello ${this.first} ${this.second}`;
   }
 }
 
 export class WeatherLocation {
-  constructor(weather, city) {
+  constructor(private weather: string, private city: string) {
     this.weather = weather;
     this.city = city;
   }
 
-  get weatherMessage() {
+  get weatherMessage(): string {
     return `It is ${this.weather} in ${this.city}`;
   }
 }
