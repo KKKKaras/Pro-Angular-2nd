405ee214a17a988aaf1bbc0c1a15bf7399b02872
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-07 13:50:35 +0100

    USING VARIABLE CLOSURE

--- 05 - JavaScript and TypeScript - Part 1/JavaScriptPrimer/primer.ts
+++ 05 - JavaScript and TypeScript - Part 1/JavaScriptPrimer/primer.ts
@@ -1,27 +1,9 @@
-let messageFunction = function(name, weather) {
-  let message = "Hello, Peter";
-  if (weather == "sunny") {
-    let message = "It is a nice day";
-    console.log(message);
-  } else {
-    let message = "It is " + weather + " today";
-    console.log(message);
-  }
-  console.log(message);
+let myGlobalVar = "apples";
+let myFunc = function(name) {
+  let myLocalVar = "sunny";
+  let innerFunction = function() {
+    return "Hello " + name + ". Today is " + myLocalVar + ".";
+  };
+  return innerFunction();
 };
-
-let messageFunctionVar = function(name, weather) {
-  var message = "Hello, Adam";
-  if (weather == "sunny") {
-    var message = "It is a nice day";
-    console.log(message);
-  } else {
-    var message = "It is " + weather + " today";
-    console.log(message);
-  }
-  console.log(message);
-};
-
-messageFunctionVar("Adam", "raining");
-
-messageFunction("Peter", "raining");
+console.log(myFunc("Adam"));
