e9095756cb447798bd28c888851534314171f59d
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-07 15:16:46 +0100

    6.3.2.2 Renaming Imports

--- 06 - JavaScript and TypeScript - Part 2/JavaScriptPrimer/primer.ts
+++ 06 - JavaScript and TypeScript - Part 2/JavaScriptPrimer/primer.ts
@@ -1,7 +1,10 @@
 ﻿import { Name, WeatherLocation } from "./modules/NameAndWeather";
+import { Name as OtherName } from "./modules/DuplicateName";
 
 let name = new Name("Peter", "Csontos");
 let loc = new WeatherLocation("sunny", "Budapest");
+let other = new OtherName();
 
 console.log(name.nameMessage);
 console.log(loc.weatherMessage);
+console.log(other.message);
