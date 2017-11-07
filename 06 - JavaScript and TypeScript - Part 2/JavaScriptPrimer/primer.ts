5dfec77a1c1b3cd854df6ab186b9a62fea8eaa30
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-07 15:42:08 +0100

    6.4.1 Using Type Annotations

--- 06 - JavaScript and TypeScript - Part 2/JavaScriptPrimer/primer.ts
+++ 06 - JavaScript and TypeScript - Part 2/JavaScriptPrimer/primer.ts
@@ -1,10 +1,14 @@
 ﻿import * as NameAndWeatherLocation from "./modules/NameAndWeather";
 import { Name as OtherName } from "./modules/DuplicateName";
+import { TempConverter } from "./tempConverter";
 
 let name = new NameAndWeatherLocation.Name("Peter", "Csontos");
 let loc = new NameAndWeatherLocation.WeatherLocation("sunny", "Budapest");
 let other = new OtherName();
 
+let cTemp = TempConverter.convertFtoC("38");
+
 console.log(name.nameMessage);
 console.log(loc.weatherMessage);
 console.log(other.message);
+console.log(`The temp is ${cTemp}C`);
