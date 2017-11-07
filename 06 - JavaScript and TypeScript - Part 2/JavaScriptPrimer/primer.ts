317534c4cfc7988714e22841b6823002c16fbc07
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-07 15:21:11 +0100

    6.3.2.3 Importing All of the Types in a Module

--- 06 - JavaScript and TypeScript - Part 2/JavaScriptPrimer/primer.ts
+++ 06 - JavaScript and TypeScript - Part 2/JavaScriptPrimer/primer.ts
@@ -1,8 +1,8 @@
-﻿import { Name, WeatherLocation } from "./modules/NameAndWeather";
+﻿import * as NameAndWeatherLocation from "./modules/NameAndWeather";
 import { Name as OtherName } from "./modules/DuplicateName";
 
-let name = new Name("Peter", "Csontos");
-let loc = new WeatherLocation("sunny", "Budapest");
+let name = new NameAndWeatherLocation.Name("Peter", "Csontos");
+let loc = new NameAndWeatherLocation.WeatherLocation("sunny", "Budapest");
 let other = new OtherName();
 
 console.log(name.nameMessage);
