21ffeb714673dd8a6eb39ad10d247514b8b6c9f1
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-13 17:09:14 +0100

    7.4 Starting the Store

---
+++ 07 - SportStore (updated)/SportStore/src/app/store/store.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
+
+@Component({
+  selector: "store",
+  moduleId: module.id,
+  templateUrl: "./store.component.html",
+  styleUrls: ["./store.component.css"]
+})
+export class StoreComponent implements OnInit {
+  constructor(private repository: ProductRepository) {}
+
+  get products(): Product[] {
+    return this.repository.getProducts();
+  }
+
+  get categories(): string[] {
+    return this.repository.getCategories();
+  }
+
+  ngOnInit() {}
+}
