33740982d7a296f7d97a0ba0c6888878fe254234
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-14 16:44:32 +0100

    7.5.4 Creating a Custom Directive

--- 07 - SportStore (updated)/SportStore/src/app/store/store.component.ts
+++ 07 - SportStore (updated)/SportStore/src/app/store/store.component.ts
@@ -1,55 +1,44 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
-
 @Component({
   selector: "store",
   moduleId: module.id,
-  templateUrl: "./store.component.html",
-  styleUrls: ["./store.component.css"]
+  templateUrl: "store.component.html"
 })
-export class StoreComponent implements OnInit {
+export class StoreComponent {
   public selectedCategory = null;
   public productsPerPage = 4;
   public selectedPage = 1;
-
   constructor(private repository: ProductRepository) {}
-
   get products(): Product[] {
-    const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
-
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
     return this.repository
       .getProducts(this.selectedCategory)
       .slice(pageIndex, pageIndex + this.productsPerPage);
   }
-
   get categories(): string[] {
     return this.repository.getCategories();
   }
-
   changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory;
   }
-
   changePage(newPage: number) {
     this.selectedPage = newPage;
   }
-
   changePageSize(newSize: number) {
     this.productsPerPage = Number(newSize);
     this.changePage(1);
   }
-
-  get pageNumbers(): number[] {
-    return Array(
-      Math.ceil(
-        this.repository.getProducts(this.selectedCategory).length /
-          this.productsPerPage
-      )
-    )
-      .fill(0)
-      .map((x, i) => i + 1);
+  get pageCount(): number {
+    return Math.ceil(
+      this.repository.getProducts(this.selectedCategory).length /
+        this.productsPerPage
+    );
   }
-
-  ngOnInit() {}
+  // get pageNumbers(): number[] {
+  // return Array(Math.ceil(this.repository
+  // .getProducts(this.selectedCategory).length / this.productsPerPage))
+  // .fill(0).map((x, i) => i + 1);
+  // }
 }
