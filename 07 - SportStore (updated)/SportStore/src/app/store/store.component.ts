174e3ed1cc7781fd7c947d06ade26c6beed29fbf
Peter Csontos <csontos.peter@ekreta.hu>
2017-11-14 14:24:14 +0100

    7.5.2 Adding Category Selection

--- 07 - SportStore (updated)/SportStore/src/app/store/store.component.ts
+++ 07 - SportStore (updated)/SportStore/src/app/store/store.component.ts
@@ -9,14 +9,20 @@ import { ProductRepository } from "../model/product.repository";
   styleUrls: ["./store.component.css"]
 })
 export class StoreComponent implements OnInit {
+  public selectedCategory = null;
+
   constructor(private repository: ProductRepository) {}
 
   get products(): Product[] {
-    return this.repository.getProducts();
+    return this.repository.getProducts(this.selectedCategory);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
   }
 
   ngOnInit() {}
